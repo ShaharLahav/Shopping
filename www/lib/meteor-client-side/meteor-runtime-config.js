3192d664d8859fad038a49ce595981787f5ba5b9
Shahar Lahav <lahav.shahar@gmail.com>
2016-04-12 01:05:06 +0300

    Added AWS server

--- www/lib/meteor-client-side/meteor-runtime-config.js
+++ www/lib/meteor-client-side/meteor-runtime-config.js
@@ -1,4 +1,4 @@
 __meteor_runtime_config__ = {};
-__meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL = 'http://checking-checking1.meteor.com';
+__meteor_runtime_config__.DDP_DEFAULT_CONNECTION_URL = 'http://52.11.183.113';
 //'http://checking-checking.meteor.com'
 //'http://localhost:3000'
